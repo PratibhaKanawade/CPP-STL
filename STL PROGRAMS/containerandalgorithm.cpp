78fe4d48b7c87f00125ebe8b44e365b9264ab359
Pratibha Kanawade <pratibhakanawade5@gmail.com>
2026-09-20 23:18:36 +0530

    Add C++ STL programs and theory

---
+++ STL PROGRAMS/containerandalgorithm.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+
+    vector<int> v ={10,50,20,40,30};  //here vector container is used
+
+    sort(v.begin() , v.end());   //here sort() algorithm is used
+
+    for(int x: v)
+    {
+        cout<<x<<" ";
+    }
+
+    return 0;
+}
---
+++ STL THEORY/stl.md
@@ -0,0 +1,7 @@
+# Standard Template Library (STL) - Interview Questions & Answers
+
+---
+
+## 1.What is STL?
+### Answer:
+### Definition:
